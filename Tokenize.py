08f4428d305e80145f04ef2ddd94002badf4b55c
PhillHenry <PhillHenry@gmail.com>
2025-01-17 17:33:20 +0000

    Demo.

--- Tokenize.py
+++ Tokenize.py
@@ -6,13 +6,15 @@ from config import MODEL_NAME
 INPUT_TOKENIZED_DATASET = "./tokenized_gpt2_dataset"
 
 def do_tokenization(model_name: str):
-    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-    tokenizer.pad_token = tokenizer.eos_token
-    # tokenizer.padding_side = 'right'
+    tokenized_dataset = tokenize()
+    tokenized_dataset.save_to_disk(INPUT_TOKENIZED_DATASET)
+    return tokenized_dataset
 
+
+def tokenize():
     # Load a dataset
     dataset = load_dataset("imdb")  # Replace "imdb" with your dataset
-
+    tokenizer = get_tokenizer()
     # Tokenize the dataset
     def tokenize_function(examples):
         tokens = tokenizer(examples["text"], truncation=True, padding="max_length", max_length=128)
@@ -20,8 +22,15 @@ def do_tokenization(model_name: str):
         return tokens
 
     tokenized_dataset = dataset.map(tokenize_function, batched=True, remove_columns=["text"])
-    tokenized_dataset.save_to_disk(INPUT_TOKENIZED_DATASET)
     return tokenized_dataset
+
+
+def get_tokenizer():
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    tokenizer.pad_token = tokenizer.eos_token
+    # tokenizer.padding_side = 'right'
+    return tokenizer
+
 
 if __name__ == "__main__":
     do_tokenization(MODEL_NAME)
--- demonstrate.py
+++ demonstrate.py
@@ -1,12 +1,14 @@
+from datasets import DatasetDict
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from transformers import AutoModelForCausalLM
 
-from config import SAVED_MODEL
+from Tokenize import INPUT_TOKENIZED_DATASET, tokenize
+from config import SAVED_MODEL, MODEL_NAME
 
 
 def generate_text(model, tokenizer):
     # Input text
-    prompt = "Tell me about Skyfall"
+    prompt = "Tell me about the James Bond film Skyfall"
 
     # Tokenize the input
     inputs = tokenizer(prompt, return_tensors="pt")
@@ -14,7 +16,7 @@ def generate_text(model, tokenizer):
     # Generate text
     output_tokens = model.generate(
         inputs["input_ids"],
-        max_length=50,  # Limit the length of the generated text
+        max_length=100,  # Limit the length of the generated text
         num_return_sequences=1,  # Generate a single sequence
         do_sample=True,  # Enable sampling for diverse outputs
         top_k=50,  # Limit sampling to top-k tokens
@@ -27,6 +29,12 @@ def generate_text(model, tokenizer):
 
 
 if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained(SAVED_MODEL)
-    model = AutoModelForCausalLM.from_pretrained(SAVED_MODEL)
+    # tokenizer = AutoTokenizer.from_pretrained(SAVED_MODEL)
+    # tokenized_dataset = DatasetDict.load_from_disk(INPUT_TOKENIZED_DATASET)
+    # tokenizer = tokenized_dataset["train"]
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    # t = tokenize()
+    imdb_model = AutoModelForCausalLM.from_pretrained(SAVED_MODEL)
+    model = AutoModelForCausalLM.from_pretrained(MODEL_NAME)
+    generate_text(imdb_model, tokenizer)
     generate_text(model, tokenizer)
