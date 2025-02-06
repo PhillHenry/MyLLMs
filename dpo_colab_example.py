14280ee3837eb3a4269af19444900d5d2a5d36a0
PhillHenry <PhillHenry@gmail.com>
2025-02-06 10:36:28 +0000

    Coherent-ish but wrong and ends with a half sentence

--- dpo_colab_example.py
+++ dpo_colab_example.py
@@ -33,7 +33,7 @@ def main(tokenization_fn):
     lora_alpha = 64
     learning_rate = 2e-6
     r = 64
-    num_epochs = 2
+    num_epochs = 3
     beta = 0.01
 
     model = FastLanguageModel.get_peft_model(
